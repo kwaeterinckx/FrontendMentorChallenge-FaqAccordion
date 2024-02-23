63d1de91b76302915949dc5dda88d55deb4c5fb9
kwaeterinckx <159497772+kwaeterinckx@users.noreply.github.com>
2024-02-23 15:58:09 +0100

    Modified script to run after page is loaded. Had issues with height calculations

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,53 @@
 // FAQ toggle
 
-// Get height of answers and put them in a custom attribute "data-height"
-const faqAnswers = document.querySelectorAll('.faq__answer');
-faqAnswers.forEach(faqAnswer => {
-    let faqAnswerHeight = faqAnswer.offsetHeight + 24;
-    faqAnswer.setAttribute("data-height", `${faqAnswerHeight}px`);
-    faqAnswer.style.height = "0px";
-});
+//* Get height and add a small bottom padding of 24px
+const getAnswerHeight = faqAnswer => {
+    let answerHeight = faqAnswer.offsetHeight + 24;
+    return answerHeight;
+};
 
-// Set click event listener to each quesion
-const faqQuestions = document.querySelectorAll('.faq__question');
-faqQuestions.forEach(faqQuestion => {
-    // On click
-    faqQuestion.addEventListener('click', () => {
-        // Remove toggle when clicking another link
-        faqQuestions.forEach(faqOpenQuestion => {
-            if (faqOpenQuestion !== faqQuestion) {
-                // Remove classes
-                faqOpenQuestion.classList.remove("faq__question--open");
-                faqOpenQuestion.nextElementSibling.classList.remove("faq__answer--open");
-                // Set height back to 0
+
+
+window.addEventListener("load", () => {
+
+    //* Get height of all answers and set them as a custom attribute (data-height)
+    const faqAnswers = document.querySelectorAll('.faq__answer');
+    const faqQuestions = document.querySelectorAll('.faq__question');
+
+    faqAnswers.forEach(faqAnswer => {
+        faqAnswer.setAttribute("data-height", `${getAnswerHeight(faqAnswer)}px`);
+        faqAnswer.style.height = 0;
+    });
+
+    //* Add click eventlistener on all questions
+    faqQuestions.forEach(faqQuestion => {
+        //* On click
+        faqQuestion.addEventListener('click', () => {
+            /**
+             ** Remove toggle class on not clicked questions
+             ** to make the toggle work when clicking the same question to close it
+            **/
+            faqQuestions.forEach(faqOpenQuestion => {
+                if (faqOpenQuestion !== faqQuestion) {
+                    faqOpenQuestion.classList.remove('faq__question--open');
+                    faqOpenQuestion.nextElementSibling.classList.remove("faq__answer--open");
+                }
+                //* Set height back to 0
                 faqAnswers.forEach(faqOpenAnswer => {
                     faqOpenAnswer.style.height = "0px";
                 });
+            });
+
+            //* Toggle question & answer class
+            faqQuestion.classList.toggle("faq__question--open");
+            faqQuestion.nextElementSibling.classList.toggle("faq__answer--open");
+
+            //* Set height on selected question
+            const faqOpenAnswer = document.querySelector(".faq__answer--open");
+            if (faqOpenAnswer) {
+                faqOpenAnswer.style.height = faqOpenAnswer.getAttribute("data-height");
             }
         });
-        // Toggle clicked question
-        faqQuestion.classList.toggle("faq__question--open");
-        faqQuestion.nextElementSibling.classList.toggle("faq__answer--open");
-
-        // Set height on open question
-        const faqOpenAnswer = document.querySelector('.faq__answer--open');
-        if (faqOpenAnswer) {
-            faqOpenAnswer.style.height = faqOpenAnswer.getAttribute("data-height");
-        }
     });
 });
+
